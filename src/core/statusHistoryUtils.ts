6fc235d294eab52bcdc56dc623b8f08c5a2cd3e7
Jesse David Peterson <jesdavpet@users.noreply.github.com>
2026-06-30 23:44:08 -0300

    fix(canvas): replace getFieldDisplayLinks shim with actual getDataLinks implementation from status-history

--- src/core/statusHistoryUtils.ts
+++ src/core/statusHistoryUtils.ts
@@ -1,3 +1,24 @@
 // SOURCE: https://github.com/grafana/grafana/blob/main/public/app/plugins/panel/status-history/utils.ts
-// TODO: Delete this shim once CanvasTooltip is updated to import getFieldDisplayLinks directly from @grafana/ui
-export { getFieldDisplayLinks as getDataLinks } from '@grafana/ui';
+// TODO: Publish getDataLinks from @grafana/ui and delete this duplicate file
+import { type Field, type LinkModel } from '@grafana/data';
+
+export const getDataLinks = (field: Field, rowIdx: number) => {
+  const links: Array<LinkModel<Field>> = [];
+
+  if ((field.config.links?.length ?? 0) > 0 && field.getLinks != null) {
+    const v = field.values[rowIdx];
+    const disp = field.display ? field.display(v) : { text: `${v}`, numeric: +v };
+
+    const linkLookup = new Set<string>();
+
+    field.getLinks({ calculatedValue: disp, valueRowIndex: rowIdx }).forEach((link) => {
+      const key = `${link.title}/${link.href}`;
+      if (!linkLookup.has(key)) {
+        links.push(link);
+        linkLookup.add(key);
+      }
+    });
+  }
+
+  return links;
+};
