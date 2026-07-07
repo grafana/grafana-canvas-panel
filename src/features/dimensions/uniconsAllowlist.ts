83577fc5cfc09e9286f9504de33d703d39d6c3d3
Jesse David Peterson <jesdavpet@users.noreply.github.com>
2026-07-07 12:32:38 -0300

    chore(canvas): remove unused uniconsAllowlist.ts (orphaned after FolderPickerTab manifest refactor)

--- src/features/dimensions/uniconsAllowlist.ts
+++
@@ -1,7 +0,0 @@
-export enum AllowedUnicon {
-  CheckCircle = 'check-circle',
-  Cloud = 'cloud',
-  ExclamationTriangle = 'exclamation-triangle',
-  QuestionCircle = 'question-circle',
-  TimesCircle = 'times-circle',
-}
